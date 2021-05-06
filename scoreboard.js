95bbe76776d322f2347b679d1bd722f839702c57
Rida <mrida@live.unc.edu>
2021-05-06 03:23:02 -0400

    fge

--- scoreboard.js
+++ scoreboard.js
@@ -5,26 +5,37 @@ $(document).ready(async (e) => {
     let darkSwitch = $('#darkSwitch');
 
     darkSwitch.prop('checked', isDark());
-    if(isDark()){
+    if (isDark()) {
         darkened.addClass('dark');
-    } else{
+    } else {
         darkened.removeClass('dark');
     };
 
-    darkSwitch.on('change', (e)=> {
+    darkSwitch.on('change', (e) => {
         toggle();
-        if(isDark()){
+        if (isDark()) {
             darkened.addClass('dark');
-            console.log("is dark must be true,"+isDark());
-        } else{
+            console.log("is dark must be true," + isDark());
+        } else {
             darkened.removeClass('dark');
-            console.log("is dark must be false,"+isDark());
+            console.log("is dark must be false," + isDark());
         }
     });
 
+    // add your score
+    //let updateScore = {
+        // "url": "http://35.196.236.79/score/" + window.localStorage.getItem("username"),
+    //     "url": "http://35.196.236.79/score/selam",
+    //     "method": "PUT",
+    //     "body": {
+    //         "score": window.localStorage.getItem("timer")
+    //     }
+    // };
+    //await $.ajax(updateScore).done();
 
+    // get scoreboard
     let scores = {
-        "url": "http://localhost:8080/scoreboard",
+        "url": "http://35.196.236.79/scoreboard",
         "method": "GET",
     };
     let scoreSet;
@@ -32,12 +43,39 @@ $(document).ready(async (e) => {
         scoreSet = response;
     });
 
+    let scoresUpTo = 20;
+    $('pageCount').html('Page Number '+scoresUpTo/20 +' out of '+scoreSet.length/20);
     $('scoreboardDiv').empty();
-    for(let i=0; i<20; i++){
-        $('scoreboardDiv').append("<div class=scoreline> <div class=user>"+ scoreSet[i].username+ "</div> <div class=score>"+ scoreSet[i].highest_score+ "</div> </div>");
+    for (let i = 0; i < scoresUpTo; i++) {
+        $('scoreboardDiv').append("<div class=scoreline> <div class=user>" + scoreSet[i].username + "</div> <div class=score>" + scoreSet[i].highest_score + "</div> </div>");
     }
-    
 
+    $('#nextScore').on('click', () => {
+        scoresUpTo += 20;
+        if (scoresUpTo >= scoreSet.length) {
+            scoresUpTo -= 20;
+            alert("That is all the scores, next page does not exist.")
+        }
+        else {
+            $('scoreboardDiv').empty();
+            for (let i = scoresUpTo - 20; i < scoresUpTo; i++) {
+                $('scoreboardDiv').append("<div class=scoreline> <div class=user>" + scoreSet[i].username + "</div> <div class=score>" + scoreSet[i].highest_score + "</div> </div>");
+            }
+        }
+    });
 
+    $('#prevScore').on('click', () => {
+        scoresUpTo -= 20;
+        if (scoresUpTo < 20) {
+            scoresUpTo += 20;
+            alert("This is the first page, previous page does not exist.")
+        }
+        else {
+            $('scoreboardDiv').empty();
+            for (let i = scoresUpTo - 20; i < scoresUpTo; i++) {
+                $('scoreboardDiv').append("<div class=scoreline> <div class=user>" + scoreSet[i].username + "</div> <div class=score>" + scoreSet[i].highest_score + "</div> </div>");
+            }
+        }
+    });
 
 });
