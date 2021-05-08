627c7bc9b7c8ffe1bbe90534c0a7dd8639df29f1
Rida <mrida@live.unc.edu>
2021-05-07 21:37:51 -0400

    fix inshallah

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $(document).ready(async (e) => {
             darkened.removeClass('dark');
         }
     });
-    
+    timerCount = setInterval(function () { timer(); }, 1000);
 });
 
 let timerArray = ["", "Start", "1", "2"]
@@ -68,65 +68,65 @@ let dishArray = ['soup', 'appetizer', 'main course', 'side dish', 'salad', 'drin
 let dishCount = -1;
 async function gamePlay() {
 
-    for (let i = 1; i < 5; i++) { 
+    for (let i = 1; i < 5; i++) {
         $('#choice' + i).remove();
     }
 
-$('#gameDiv').append('<button class="choice" id="choice1"></button>');
-$('#gameDiv').append('<button class="choice" id="choice2"></button>');
-$('#gameDiv').append('<button class="choice" id="choice3"></button>');
-$('#gameDiv').append('<button class="choice" id="choice4"></button>');
+    $('#gameDiv').append('<button class="choice" id="choice1"></button>');
+    $('#gameDiv').append('<button class="choice" id="choice2"></button>');
+    $('#gameDiv').append('<button class="choice" id="choice3"></button>');
+    $('#gameDiv').append('<button class="choice" id="choice4"></button>');
 
 
-dishCount++;
-$('#mealType').html('Choosing for ' + dishArray[dishCount]);
-let vegetarianSet = await {
-    "url": "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian," + dishArray[dishCount] + "&apiKey=2be809f1357046c5a9ee30442c8912a0",
-    "method": "GET",
-    "timeout": 0
-};
-let vegetarian;
-await $.ajax(vegetarianSet).done(function (response) {
-    vegetarian = response.recipes[0];
-});
-console.log(vegetarian);
+    dishCount++;
+    $('#mealType').html('Choosing for ' + dishArray[dishCount]);
+    let vegetarianSet = await {
+        "url": "https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian," + dishArray[dishCount] + "&apiKey=2be809f1357046c5a9ee30442c8912a0",
+        "method": "GET",
+        "timeout": 0
+    };
+    let vegetarian;
+    await $.ajax(vegetarianSet).done(function (response) {
+        vegetarian = response.recipes[0];
+    });
+    console.log(vegetarian);
 
-let restSet = await {
-    "url": "https://api.spoonacular.com/recipes/random?number=3&tags=" + dishArray[dishCount] + "&apiKey=2be809f1357046c5a9ee30442c8912a0",
-    "method": "GET",
-    "timeout": 0
-};
+    let restSet = await {
+        "url": "https://api.spoonacular.com/recipes/random?number=3&tags=" + dishArray[dishCount] + "&apiKey=2be809f1357046c5a9ee30442c8912a0",
+        "method": "GET",
+        "timeout": 0
+    };
 
-let rest;
-await $.ajax(restSet).done(function (response) {
-    rest = response;
-});
-console.log(rest);
+    let rest;
+    await $.ajax(restSet).done(function (response) {
+        rest = response;
+    });
+    console.log(rest);
 
-let random = Math.floor(Math.random() * 3);
-random++;
-let restCount = 0;
+    let random = Math.floor(Math.random() * 3);
+    random++;
+    let restCount = 0;
 
-for (let i = 1; i <= 4; i++) {
-    let recepie;
-    if (i === random) {
-        recepie = vegetarian;
-    } else {
-        recepie = rest.recipes[restCount];
-        restCount++;
+    for (let i = 1; i <= 4; i++) {
+        let recepie;
+        if (i === random) {
+            recepie = vegetarian;
+        } else {
+            recepie = rest.recipes[restCount];
+            restCount++;
+        }
+
+        $('#choice' + i).html(recepie.title);
+        $('#choice' + i).append(`<img class="foodImg" src="${recepie.image}">`);
+        $('#choice' + i).on('click', (e) => { handleChoice(e, recepie) });
     }
-
-    $('#choice' + i).html(recepie.title);
-    $('#choice' + i).append(`<img class="foodImg" src="${recepie.image}">`);
-    $('#choice' + i).on('click', (e) => { handleChoice(e, recepie) });
-}
 }
 
 function handleChoice(event, recepie) {
     if (recepie.vegetarian) {
         score += 10;
         $('#gamePoints').html("Score: " + score);
-        if (dishCount >= dishArray.length-1) {
+        if (dishCount >= dishArray.length - 1) {
             console.log("you win!");
             window.localStorage.setItem("timer", gameTimerCount);
             window.location.replace("win.html");
